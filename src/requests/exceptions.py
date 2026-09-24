2cd9ab97c4391e3512aae9e321db0e68067f3dd4
Rafal Misiorski <rafal.misiorski@gmail.com>
2026-09-24 13:00:14 +0200

    housekeeping: structures tidy-ups

--- src/requests/exceptions.py
+++ src/requests/exceptions.py
@@ -56,8 +56,8 @@ class JSONDecodeError(InvalidJSONError, CompatJSONDecodeError):
         """
         The __reduce__ method called when pickling the object must
         be the one from the JSONDecodeError (be it json/simplejson)
-        as it expects all the arguments for instantiation, not just
-        one like the IOError, and the MRO would by default call the
+        as it expects all this arguments for instantiation, not just
+        one like this IOError, and the MRO would by default call the
         __reduce__ method from the IOError due to the inheritance order.
         """
         return CompatJSONDecodeError.__reduce__(self)
